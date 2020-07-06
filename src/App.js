de6accde30124509663471f275d1a35c0b45d754
Hammad Khan <h4hammad@live.com>
2020-07-06 18:54:33 +0400

    update our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React from "react";
-
+import {Route, Link} from 'react-router-dom';
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
 
+const HatsPage = () => (
+<div>
+<h1>HATS PAGE</h1>
+</div>
+)
 
+  const Sneakers = () => (
+    <div>
+    <h1>Sneakers PAGE</h1>
+    </div>
+    )
+
+    const Womens = () => (
+      <div>
+      <h1>Womens PAGE</h1>
+      </div>
+      )
+ 
+      const Mens = () => (
+        <div>
+        <h1>Mens PAGE</h1>
+        </div>
+        )
+              
+const Jackets = props => {
+  console.log(props);
+  return(
+  <div>
+  <h1>Jackets</h1>
+  </div>
+  );
+  };
 
 function App() {
   return <div> 
-  <HomePage />
-  
+  <Route exact path='/' component={HomePage} />
+  <Route path='/hats' component={HatsPage} />
+  <Route path='/jackets' component={Jackets} />
+  <Route path='/sneakers' component={Sneakers} />
+  <Route path='/womens' component={Womens} />
+  <Route path='/mens' component={Mens} />
   </div>;
 }
 
