c207bebc5122be37a677850d6e7b7c17d3bf6bc3
Hammad Khan <h4hammad@live.com>
2020-07-07 18:27:49 +0400

    Header menu and logo added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import {Route, Link} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
-
+import Header from './components/hearder/header.component.jsx'
 
 function App() {
-  return <div> 
-  <Route exact path='/' component={HomePage} />
-  <Route path='/shop' component={ShopPage} />
-  </div>;
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  )
+};
 
 export default App;
